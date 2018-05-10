f2e8e329ed83eca5bacbb8381f3a000052d286c7
kubzdel <kubzdel@gmail.com>
2018-05-10 16:19:58 +0200

    Finished.

--- src/Compression.java
+++ src/Compression.java
@@ -1,33 +1,30 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Compression {
 
     private String sourceText;
     private HashMap<Character,String> code;
-
+    private HashMap<String,Character> loadedCode;
+    private String loadedText;
+    private int encodedTextLength;
+    private int loadedTextLength;
+    private ArrayList<String>loadedTextList= new ArrayList<>();
+    private int loadedCodeLength;
+    private String encodedText;
 
 
     public void create(String text)
     {
         int n =0;
         HashMap<Character,String> code = new HashMap();
-        for(int i=0;i<text.length();i++)
-        {
-            if(!code.containsKey(text.charAt(i)))
-            {
+        for(int i=0;i<text.length();i++) {
+            if (!code.containsKey(text.charAt(i))) {
                 String codeN = Integer.toBinaryString(n);
-                code.put(text.charAt(i),codeN);
+                code.put(text.charAt(i), codeN);
                 n++;
             }
         }
-        System.out.println(123);
         int size = (int) (Math.log(code.size())/Math.log(2)) +1;
         for (HashMap.Entry<Character,String> entry : code.entrySet()) {
             String newKey = entry.getValue();
@@ -36,53 +33,139 @@ public class Compression {
                 newKey = "0"+ newKey;
             }
             code.replace(entry.getKey(),newKey);
-            System.out.println(newKey.getBytes().length);
         }
         this.code = code;
     }
 
 
-    public byte[] encode(String text)
+
+    public BitSet encode(String text)
     {
-        ArrayList<Byte> byteList = new ArrayList();
-        String encoded="";
+        ArrayList<String> encodedList = new ArrayList<>();
         for(int i=0;i<text.length();i++)
         {
-            encoded=encoded+code.get(text.charAt(i));
+            encodedList.add(code.get(text.charAt(i)));
         }
-        for(int i=0;i<encoded.length()-8;i++)
+
+        encodedTextLength = encodedList.size();
+        int k=0;
+        BitSet bitSet = new BitSet();
+        for(String batch:encodedList)
         {
-            String newByte = encoded.substring(i,i+7);
-            byte b = Byte.parseByte(newByte, 2);
-            byteList.add(b);
+            for(int i=0;i<batch.length();i++)
+            {
+                if(batch.charAt(i)=='1')
+                {
+                    bitSet.set(k);
+                }
+                k++;
+            }
         }
-        byte[] result = new byte[byteList.size()];
-        for(int i = 0; i < byteList.size(); i++) {
-            result[i] = byteList.get(i).byteValue();
-        }
-        return result;
-
-
+        return bitSet;
     }
 
-    public void save(byte[] data) throws IOException {
-        File outputFile = new File("zakodowany");
+    public void save(BitSet data) throws IOException {
+        byte[] bytes = data.toByteArray();
+        File outputFile = new File("zakodowany.txt");
         FileOutputStream fos = new FileOutputStream(outputFile);
-        fos.write(data, 0, data.length);
+        fos.write(bytes);
         fos.flush();
         fos.close();
+
+        Writer wr = new FileWriter("dlugosc.txt");
+        wr.write(encodedTextLength+"");
+        wr.close();
+
+        FileOutputStream fileOut =  new FileOutputStream("mapping.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(code);
+        out.close();
+        fileOut.close();
     }
+
+    public void load() throws IOException, ClassNotFoundException {
+
+        FileInputStream map = new FileInputStream("mapping.ser");
+        ObjectInputStream in = new ObjectInputStream(map);
+        HashMap<Character,String> pom = new HashMap();
+        pom = (HashMap<Character, String>) in.readObject();
+        loadedCodeLength = pom.values().iterator().next().length();
+        HashMap<String, Character> myNewHashMap = new HashMap<>();
+        for(Map.Entry<Character, String> entry : pom.entrySet()){
+            myNewHashMap.put(entry.getValue(), entry.getKey());
+        }
+        loadedCode = myNewHashMap;
+        in.close();
+        map.close();
+
+
+        File file = new File("zakodowany.txt");
+        byte[] fileData = new byte[(int) file.length()];
+        DataInputStream dis = new DataInputStream(new FileInputStream(file));
+        dis.readFully(fileData);
+        dis.close();
+        ArrayList<String>loadedBatches = new ArrayList<>();
+        int k=0;
+        StringBuilder batches = new StringBuilder();
+        for(byte block:fileData)
+        {
+            if(k%10000==0){
+                double proc = (k / (double) fileData.length) * 100;
+                System.out.println(proc);}
+                String temp = Integer.toBinaryString((block + 256) % 256);// output -> "10000010
+
+                while (temp.length() < 8) {
+                    temp = '0' + temp;
+                }
+                temp = new StringBuilder(temp).reverse().toString();
+                batches.append(temp);
+
+        k++;
+        }
+        loadedTextList= loadedBatches;
+        loadedText= batches.toString();
+
+        encodedTextLength =Integer.valueOf(new Scanner(new File("dlugosc.txt")).useDelimiter("\\Z").next());
+    }
+
+    public String decode(String text){
+        StringBuilder decodedText=new StringBuilder();
+        for(int k=0;k<encodedTextLength;k++) {
+
+                decodedText.append(loadedCode.get(text.substring(k*loadedCodeLength,k * loadedCodeLength+loadedCodeLength)));
+        }
+        System.out.println(sourceText.substring(sourceText.length()-50));
+        System.out.println(decodedText.substring(decodedText.length()-50));
+        return decodedText.toString();
+
+    }
+
+    public void validate(String sourceText,String decodedText)
+    {
+        if(sourceText.equals(decodedText))
+        {
+            System.out.println("Tekst został zakodowany i odkodowany poprawanie");
+        }
+        else
+        {
+            System.out.println("Tekst został niepoprawnie odkodowany!");
+        }
+    }
+
     public void process() throws FileNotFoundException {
-        Scanner scanner = new Scanner( new File("test.txt") );
+        Scanner scanner = new Scanner( new File("norm_wiki_sample.txt") );
         sourceText = scanner.useDelimiter("\\A").next();
+        if(sourceText.charAt(sourceText.length()-1)==' ')
+        sourceText = sourceText.substring(0,sourceText.length()-1);
         create(sourceText);
         try {
             save(encode(sourceText));
+            load();
+            validate(sourceText,decode(loadedText));
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-
-
-
 }
